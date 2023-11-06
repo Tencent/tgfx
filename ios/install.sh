d2629023a464a859a921023b6cf69cc146efd7a1
domrjchen <dom@idom.me>
2023-11-06 15:37:29 +0800

    Rename TGFXDemo to Hello2D.

--- ios/install.sh
+++ ios/install.sh
@@ -7,7 +7,7 @@ cd $(dirname $0)
 cmake -G Xcode -B tgfx .. -DCMAKE_TOOLCHAIN_FILE=../../third_party/vendor_tools/ios.toolchain.cmake -DPLATFORM=OS64 -DENABLE_ARC=OFF
 
 # Prepare the workspace in the TEMP directory
-NAME="TGFXDemo.xcworkspace"
+NAME="Hello2D.xcworkspace"
 DATA_FILE="contents.xcworkspacedata"
 TEMP_DIR=$(mktemp -d)
 mkdir "${TEMP_DIR}/${NAME}"
@@ -15,7 +15,7 @@ touch "${TEMP_DIR}/${NAME}/${DATA_FILE}"
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" >> "${TEMP_DIR}/${NAME}/${DATA_FILE}"
 echo "<Workspace version = \"1.0\">" >> "${TEMP_DIR}/${NAME}/${DATA_FILE}"
-echo "  <FileRef location = \"group:TGFXDemo.xcodeproj\"></FileRef>" >> "${TEMP_DIR}/${NAME}/${DATA_FILE}"
+echo "  <FileRef location = \"group:Hello2D.xcodeproj\"></FileRef>" >> "${TEMP_DIR}/${NAME}/${DATA_FILE}"
 echo "  <FileRef location = \"group:tgfx/TGFX.xcodeproj\"></FileRef>" >> "${TEMP_DIR}/${NAME}/${DATA_FILE}"
 echo "</Workspace>" >> "${TEMP_DIR}/${NAME}/${DATA_FILE}"
 
