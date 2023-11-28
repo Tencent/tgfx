4f5c7f8a60b7fed8fd92bde7f8db4b30d5159cf4
domrjchen <dom@idom.me>
2023-11-28 22:57:32 +0800

    Update some scripts.

--- web/script/install-emscripten.sh
+++ web/script/install-emscripten.sh
@@ -5,7 +5,7 @@ INSTALL_PATH="/usr/local/Cellar/emsdk"
 echo "-----install emscripten begin-----"
 if [ ! -d $INSTALL_PATH ]; then
   mkdir -p $INSTALL_PATH
- git clone https://github.com/emscripten-core/emsdk.git $INSTALL_PATH
+  git clone https://github.com/emscripten-core/emsdk.git $INSTALL_PATH
 fi
 
 "$INSTALL_PATH/emsdk" install 3.1.20
