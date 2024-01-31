01aee050d5fa5ca9eb33329e9e37f9f203a3991b
Dom Chen <dom@idom.me>
2024-01-31 15:59:00 +0800

    Improve performance by enabling multi-threading for the built-in freetype library. (#133)

--- src/vectors/freetype/FTTypeface.cpp
+++ src/vectors/freetype/FTTypeface.cpp
@@ -17,9 +17,8 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "FTTypeface.h"
-
+#include "FTLibrary.h"
 #include FT_TRUETYPE_TABLES_H
-
 #include "FTScalerContext.h"
 #include "SystemFont.h"
 #include "tgfx/utils/UTF.h"
@@ -118,23 +117,54 @@ std::shared_ptr<Typeface> Typeface::MakeDefault() {
   return std::make_shared<EmptyTypeface>();
 }
 
+static std::mutex& FTMutex() {
+  static std::mutex& mutex = *new std::mutex;
+  return mutex;
+}
+
+static FT_Face CreateFTFace(const FTFontData& data) {
+  std::lock_guard<std::mutex> autoLock(FTMutex());
+  FT_Open_Args args;
+  memset(&args, 0, sizeof(args));
+  if (data.data) {
+    args.flags = FT_OPEN_MEMORY;
+    args.memory_base = static_cast<const FT_Byte*>(data.data->data());
+    args.memory_size = static_cast<FT_Long>(data.data->size());
+  } else if (!data.path.empty()) {
+    args.flags = FT_OPEN_PATHNAME;
+    args.pathname = const_cast<FT_String*>(data.path.c_str());
+  } else {
+    return nullptr;
+  }
+  FT_Face face = nullptr;
+  auto ftLibrary = FTLibrary::Get();
+  auto err = FT_Open_Face(ftLibrary, &args, data.ttcIndex, &face);
+  if (err || !face->family_name) {
+    return nullptr;
+  }
+  if (!face->charmap) {
+    FT_Select_Charmap(face, FT_ENCODING_MS_SYMBOL);
+  }
+  return face;
+}
+
 std::shared_ptr<FTTypeface> FTTypeface::Make(FTFontData data) {
-  auto face = FTFace::Make(data);
+  auto face = CreateFTFace(data);
   if (face == nullptr) {
     return nullptr;
   }
-  auto typeface = std::shared_ptr<FTTypeface>(new FTTypeface(std::move(data), std::move(face)));
+  auto typeface = std::shared_ptr<FTTypeface>(new FTTypeface(std::move(data), face));
   typeface->weakThis = typeface;
   return typeface;
 }
 
-FTTypeface::FTTypeface(FTFontData data, std::unique_ptr<FTFace> face)
-    : _uniqueID(UniqueID::Next()), data(std::move(data)), _face(std::move(face)) {
+FTTypeface::FTTypeface(FTFontData data, FT_Face face)
+    : _uniqueID(UniqueID::Next()), data(std::move(data)), face(std::move(face)) {
 }
 
 FTTypeface::~FTTypeface() {
-  std::lock_guard<std::mutex> lockGuard(FTMutex());
-  _face = nullptr;
+  std::lock_guard<std::mutex> autoLock(FTMutex());
+  FT_Done_Face(face);
 }
 
 int FTTypeface::GetUnitsPerEm(FT_Face face) {
@@ -149,20 +179,34 @@ int FTTypeface::GetUnitsPerEm(FT_Face face) {
   return unitsPerEm;
 }
 
+std::string FTTypeface::fontFamily() const {
+  std::lock_guard<std::mutex> autoLock(locker);
+  return face->family_name ? face->family_name : "";
+}
+
+std::string FTTypeface::fontStyle() const {
+  std::lock_guard<std::mutex> autoLock(locker);
+  return face->style_name ? face->style_name : "";
+}
+
 int FTTypeface::glyphsCount() const {
-  return static_cast<int>(_face->face->num_glyphs);
+  std::lock_guard<std::mutex> autoLock(locker);
+  return static_cast<int>(face->num_glyphs);
 }
 
 int FTTypeface::unitsPerEm() const {
-  return GetUnitsPerEm(_face->face);
+  std::lock_guard<std::mutex> autoLock(locker);
+  return GetUnitsPerEm(face);
 }
 
 bool FTTypeface::hasColor() const {
-  return FT_HAS_COLOR(_face->face);
+  std::lock_guard<std::mutex> autoLock(locker);
+  return FT_HAS_COLOR(face);
 }
 
 GlyphID FTTypeface::getGlyphID(Unichar unichar) const {
-  return static_cast<GlyphID>(FT_Get_Char_Index(_face->face, static_cast<FT_ULong>(unichar)));
+  std::lock_guard<std::mutex> autoLock(locker);
+  return static_cast<GlyphID>(FT_Get_Char_Index(face, static_cast<FT_ULong>(unichar)));
 }
 
 std::shared_ptr<Data> FTTypeface::getBytes() const {
@@ -170,12 +214,7 @@ std::shared_ptr<Data> FTTypeface::getBytes() const {
 }
 
 std::shared_ptr<Data> FTTypeface::copyTableData(FontTableTag tag) const {
-  std::lock_guard<std::mutex> lockGuard(FTMutex());
-  auto face = _face->face;
-  if (face == nullptr) {
-    return nullptr;
-  }
-
+  std::lock_guard<std::mutex> autoLock(locker);
   FT_ULong tableLength = 0;
   auto error = FT_Load_Sfnt_Table(face, tag, 0, nullptr, &tableLength);
   if (error) {
