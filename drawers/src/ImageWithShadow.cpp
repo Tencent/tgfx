67378bd445218617ff9f33e0aa331cb1f89b903b
Dom Chen <dom@idom.me>
2024-07-08 20:45:58 +0800

    Add the Hello2D demo for the OHOS platform.

--- drawers/src/ImageWithShadow.cpp
+++ drawers/src/ImageWithShadow.cpp
@@ -28,6 +28,9 @@ void ImageWithShadow::onDraw(tgfx::Canvas* canvas, const drawers::AppHost* host)
   auto size = screenSize - static_cast<int>(150 * scale);
   size = std::max(size, 50);
   auto image = host->getImage("bridge");
+  if (image == nullptr) {
+    return;
+  }
   image = image->makeMipmapped(true);
   auto imageScale = static_cast<float>(size) / static_cast<float>(image->width());
   auto matrix = tgfx::Matrix::MakeScale(imageScale);
