838d62d18c2c07e05bc64163a17cb3f2417e7e06
Dom Chen <dom@idom.me>
2024-10-17 09:59:10 +0800

    Add support for creating an Image from the given Picture.

---
+++ src/core/images/PictureImage.h
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Tencent is pleased to support the open source community by making tgfx available.
+//
+//  Copyright (C) 2024 THL A29 Limited, a Tencent company. All rights reserved.
+//
+//  Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+//  in compliance with the License. You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/BSD-3-Clause
+//
+//  unless required by applicable law or agreed to in writing, software distributed under the
+//  license is distributed on an "as is" basis, without warranties or conditions of any kind,
+//  either express or implied. see the license for the specific language governing permissions
+//  and limitations under the license.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "core/images/ResourceImage.h"
+
+namespace tgfx {
+/**
+ * PictureImage
+ */
+class PictureImage : public ResourceImage {
+ public:
+  static std::shared_ptr<Image> MakeFrom(std::shared_ptr<Picture> picture, int width, int height,
+                                         const Matrix* matrix = nullptr,
+                                         const Paint* paint = nullptr);
+
+  ~PictureImage() override;
+
+  int width() const override {
+    return _width;
+  }
+
+  int height() const override {
+    return _height;
+  }
+
+  bool isAlphaOnly() const override {
+    return false;
+  }
+
+ protected:
+  std::shared_ptr<TextureProxy> onLockTextureProxy(const TPArgs& args) const override;
+
+ private:
+  std::shared_ptr<Picture> picture = nullptr;
+  int _width = 0;
+  int _height = 0;
+  Matrix* matrix = nullptr;
+  Paint* paint = nullptr;
+
+  PictureImage(UniqueKey uniqueKey, std::shared_ptr<Picture> picture, int width, int height,
+               const Matrix* matrix = nullptr, const Paint* paint = nullptr);
+};
+}  // namespace tgfx
