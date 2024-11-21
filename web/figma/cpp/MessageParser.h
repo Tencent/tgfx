a25a23278956391b9ba0a185932fa55d5ee3a13a
Fjie <ffjiefan@tencent.com>
2024-11-21 17:46:47 +0800

    Refactor the json parsing code.

--- web/figma/cpp/MessageParser.h
+++ web/figma/cpp/MessageParser.h
@@ -1,4 +1,3 @@
-
 #pragma once
 #include <string>
 #include "Element.h"
@@ -7,4 +6,14 @@
 class MessageParser {
 public:
     static bool parseMessage(const std::string& message, JsMessage& jsMessage);
+private:
+    static void parseCanvasRect(const nlohmann::json& canvasRect, JsMessage& jsMessage);
+    static void parseViewBox(const nlohmann::json& viewBox, JsMessage& jsMessage);
+    static void parseElements(const nlohmann::json& elements, JsMessage& jsMessage);
+
+    static void parseRect(const nlohmann::json& rect, JsElement& jsElem);
+    static void parseCircle(const nlohmann::json& circle, JsElement& jsElem);
+    static void parseText(const nlohmann::json& text, JsElement& jsElem);
+
+    static float getFloat(const nlohmann::json& obj, const std::string& key);
 };
