2d3d299d23d48d0a93ad7155a3f7ce2599603284
Fjie <ffjiefan@tencent.com>
2024-11-25 21:47:04 +0800

    Adjust the timing of wasm loading.

--- web/figma/ts/loadWasm.ts
+++ web/figma/ts/loadWasm.ts
@@ -1,20 +1,27 @@
-import Figma from '../wasm-mt/figma.js'; // 导入 FigmaRenderer
-// import {TGFXBind} from '../../lib/tgfx.js';
+import Figma from '../wasm-mt/figma.js';
+import * as types from "../../types/types"; // 导入 FigmaRenderer
+import {TGFXBind} from '../../lib/tgfx.js';
 
-(async () => {
-    try {
-        const figma = await Figma({
-            locateFile: (file) => './wasm-mt/' + file
-        });
+if (typeof window !== 'undefined') {
+    window.onload = async () => {
+        try {
+            const figma = await Figma({
+                locateFile: (file) => './wasm-mt/' + file
+            }).then((module: types.TGFX) => {
+                TGFXBind(module);
+                return module;
+            }).catch((error: any) => {
+                console.error(error);
+                throw new Error("Hello2D init failed. Please check the .wasm file path!.");
+            });
 
-        // 绑定 TGFX
-        // TGFXBind(figma);
 
-        // 发送自定义事件，传递 figma 模块
-        const event = new CustomEvent('wasmLoaded', { detail: figma });
-        document.dispatchEvent(event);
-    } catch (error) {
-        console.error(error);
-        throw new Error("TGFX init failed. Please check the .wasm file path!.");
+            // 发送自定义事件，传递 figma 模块
+            const event = new CustomEvent('wasmLoaded', {detail: figma});
+            document.dispatchEvent(event);
+        } catch (error) {
+            console.error(error);
+            throw new Error("TGFX init failed. Please check the .wasm file path!.");
+        }
     }
-})();
+}
