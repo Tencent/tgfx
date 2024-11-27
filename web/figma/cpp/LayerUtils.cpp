f9b57c90c9bd31e246f250e19f273f9509d067f7
Fjie <ffjiefan@tencent.com>
2024-11-27 22:40:19 +0800

    Add copyright.

--- web/figma/cpp/LayerUtils.cpp
+++ web/figma/cpp/LayerUtils.cpp
@@ -1,3 +1,21 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Tencent is pleased to support the open source community by making tgfx available.
+//
+//  Copyright (C) 2024 THL A29 Limited, a Tencent company. All rights reserved.
+//
+//  Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+//  in compliance with the License. You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/BSD-3-Clause
+//
+//  unless required by applicable law or agreed to in writing, software distributed under the
+//  license is distributed on an "as is" basis, without warranties or conditions of any kind,
+//  either express or implied. see the license for the specific language governing permissions
+//  and limitations under the license.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 #include "LayerUtils.h"
 #include <tgfx/core/Typeface.h>
 #include <tgfx/layers/ShapeLayer.h>
@@ -5,7 +23,6 @@
 #include <tgfx/layers/filters/BlurFilter.h>
 #include <chrono>  // 添加头文件用于计时
 #include <iostream>
-#include "ProjectPath.h"
 #include "utils.h"
 
 // 静态变量用于统计耗时和执行次数
--- web/figma/ts/models/CustomOnINP.ts
+++ web/figma/ts/models/CustomOnINP.ts
@@ -1,18 +1,21 @@
-/*
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Tencent is pleased to support the open source community by making tgfx available.
+//
+//  Copyright (C) 2024 THL A29 Limited, a Tencent company. All rights reserved.
+//
+//  Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+//  in compliance with the License. You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/BSD-3-Clause
+//
+//  unless required by applicable law or agreed to in writing, software distributed under the
+//  license is distributed on an "as is" basis, without warranties or conditions of any kind,
+//  either express or implied. see the license for the specific language governing permissions
+//  and limitations under the license.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 // 这个类是从 web-vitals 包中复制的，进行了一些修改，主要是为了提供一个resetINP方法，用于重置INP指标的处理；
 // 这样方便控制统计不同时间段内的INP指标，而不是全局的。
