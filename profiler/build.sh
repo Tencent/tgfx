bb9e48ec9bfe1833f001209045b43380dc06bff9
illidanstormrange <fengdyli@tencent.com>
2024-11-28 16:01:09 +0800

    TracyProfiler uses a dedicated folder to build

--- profiler/build.sh
+++ profiler/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if ! [[ -d "third_party/tracy" ]]; then
-  ./sync_deps.sh
+build_dir=$(dirname "$0")/build
+profiler_dir=$(dirname "$0")/../third_party/tracy/profiler
+
+if ! [[ -d "$(dirname "$0")/../third_party/tracy" ]]; then
+  sh $(dirname "$0")/../sync_deps.sh
 fi
 
-profiler_dir=third_party/tracy/profiler
-
-if [ ! $(which $profiler_dir/build/tracy-profiler) ]; then
-    mkdir -p $profiler_dir/build
-    cd $profiler_dir/build
-    cmake ..
-    make
+if [ ! $(which $build_dir/tracy-profiler) ]; then
+    mkdir $build_dir
+    cmake -B $build_dir -S $profiler_dir
+    make -C $build_dir
 fi
 
-$profiler_dir/build/tracy-profiler || exit 1
+$build_dir/tracy-profiler || exit 1
