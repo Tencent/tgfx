62fd8c44465ffe46bd412f4a6029c6320aa8ef3f
shlzxjp <shlzxjp@163.com>
2024-12-05 20:37:30 +0800

    Add debug log

--- test/src/VirtualTypefaceTest.cpp
+++ test/src/VirtualTypefaceTest.cpp
@@ -135,11 +135,12 @@ TGFX_TEST(VirtualTypefaceTest, DrawTextWithVirtualTypeface) {
   printf("VirtualTypefaceTest::DrawTextWithVirtualTypeface 5.1\n");
   auto textBlob = TextBlob::MakeFrom(std::move(glyphRunList));
   printf("VirtualTypefaceTest::DrawTextWithVirtualTypeface 5.2\n");
-#if 0
+
   auto paint = Paint();
   printf("VirtualTypefaceTest::DrawTextWithVirtualTypeface 5.3\n");
   paint.setColor(Color::Red());
   printf("VirtualTypefaceTest::DrawTextWithVirtualTypeface 5.4\n");
+#if 0
   canvas->drawTextBlob(textBlob, 0.0f, 0.0f, paint);
   printf("VirtualTypefaceTest::DrawTextWithVirtualTypeface 5.5\n");
   printf("VirtualTypefaceTest::DrawTextWithVirtualTypeface 6\n");
