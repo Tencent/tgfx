ddae4cf579adafc87b7900b12f7f7b297f442daa
domrjchen <dom@idom.me>
2024-12-19 12:40:55 +0800

    Remove the alphaOnly parameter from the Image::MakeFrom(Picture) method.

--- include/tgfx/core/Image.h
+++ include/tgfx/core/Image.h
@@ -104,7 +104,7 @@ class Image {
                                          YUVColorSpace colorSpace = YUVColorSpace::BT601_LIMITED);
 
   /**
-   * Creates an Image from the given picture with the specified width, height, and alphaOnly flag.
+   * Creates an Image from the given picture with the specified width, height, and matrix.
    * The picture will be drawn onto the Image using the provided matrix. The picture is not
    * immediately turned into raster pixels; instead, the returned Image holds a reference to the
    * picture and defers rasterization until it is actually required. Note: This method may return a
@@ -113,12 +113,11 @@ class Image {
    * @param width The width of the Image.
    * @param height The height of the Image.
    * @param matrix A Matrix to apply transformations to the picture.
-   * @param alphaOnly A flag to indicate whether the Image is alpha only.
    * @return An Image that matches the content when the picture is drawn with the specified
    * parameters.
    */
   static std::shared_ptr<Image> MakeFrom(std::shared_ptr<Picture> picture, int width, int height,
-                                         const Matrix* matrix = nullptr, bool alphaOnly = false);
+                                         const Matrix* matrix = nullptr);
 
   /**
    * Creates an Image in the I420 format with the specified YUVData and the YUVColorSpace. Returns
