53963e0046f9e89a97ada7e8d03bb951b8ee4b37
Dom Chen <dom@idom.me>
2025-03-04 17:41:04 +0800

    Optimize the performance of the OpsCompositor::canAppend() method. (#489)

--- src/gpu/ops/RectDrawOp.h
+++ src/gpu/ops/RectDrawOp.h
@@ -35,6 +35,16 @@ struct RectPaint {
 class RectDrawOp : public DrawOp {
  public:
   /**
+   * The maximum number of non-AA rects that can be drawn in a single draw call.
+   */
+  static constexpr uint16_t MaxNumNonAARects = 2048;  // max possible: (1 << 14) - 1;
+
+  /**
+   * The maximum number of AA rects that can be drawn in a single draw call.
+   */
+  static constexpr uint16_t MaxNumAARects = 512;  // max possible: (1 << 13) - 1;
+
+  /**
    * Create a new RectDrawOp for a list of RectPaints. The returned RectDrawOp is in the local space
    * of each rect.
    */
