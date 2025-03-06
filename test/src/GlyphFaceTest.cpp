cc6d2f2edf16b0cf2d66504e1a7aacb7738f1533
shlzxjp <shlzxjp@163.com>
2025-03-06 19:21:33 +0800

    TextBlob provides an interface created from GlyphFace.

--- test/src/GlyphFaceTest.cpp
+++ test/src/GlyphFaceTest.cpp
@@ -16,6 +16,10 @@
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
+#include "tgfx/layers/DisplayList.h"
+#include "tgfx/layers/ShapeLayer.h"
+#include "tgfx/layers/SolidColor.h"
+#include "tgfx/layers/TextLayer.h"
 #include "core/utils/MathExtra.h"
 #include "tgfx/core/Canvas.h"
 #include "utils/TestUtils.h"
@@ -56,6 +60,13 @@ class CustomPathGlyphFace : public GlyphFace {
         Rect rect = Rect::MakeXYWH(5.0f, 5.0f, 40.0f, 40.0f);
         rect.scale(_scale, _scale);
         path->addOval(rect);
+        path->close();
+        return true;
+      }
+      case 100: {
+        Rect rect = Rect::MakeXYWH(0.0f, 0.0f, 100.0f, 100.0f);
+        rect.scale(_scale, _scale);
+        path->addRect(rect);
         path->close();
         return true;
       }
@@ -335,5 +346,136 @@ TGFX_TEST(GlyphFaceTest, GlyphFaceWithStyle) {
                      std::make_shared<CustomImageGlyphFace2>(typeface2, 1.0f), paint);
 
   EXPECT_TRUE(Baseline::Compare(surface, "GlyphFaceTest/GlyphFaceWithStyle"));
+}
+
+TGFX_TEST(GlyphFaceTest, MakeTextBlobWithGlyphFace) {
+  auto typeface = MakeTypeface("resources/font/NotoSansSC-Regular.otf");
+  Font font(typeface, 100);
+  Font fontText(typeface, 20);
+
+  ContextScope scope;
+  auto context = scope.getContext();
+  ASSERT_TRUE(context != nullptr);
+  auto surface = Surface::Make(context, 800, 720);
+  auto displayList = std::make_unique<DisplayList>();
+
+  auto rootLayer = Layer::Make();
+
+  // text: 找个地方吃饭
+  // GlyphID: 13917 8741 11035 14739 10228 27929
+  GlyphID glyphIDs[] = {13917, 8741, 11035, 14739, 10228, 27929};
+  Point positions[] = {
+    Point::Make(150.0f, 150.0f),
+    Point::Make(250.0f, 150.0f),
+    Point::Make(350.0f, 150.0f),
+    Point::Make(450.0f, 150.0f),
+    Point::Make(550.0f, 150.0f),
+    Point::Make(650.0f, 150.0f)
+  };
+  auto textBlob = TextBlob::MakeFrom(glyphIDs, positions, 6, GlyphFace::Wrap(font));
+  auto textShape = Shape::MakeFrom(textBlob);
+
+  auto paint = Paint();
+  paint.setStyle(PaintStyle::Stroke);
+  paint.setColor(Color::Red());
+
+  auto shapeLayer1 = ShapeLayer::Make();
+  shapeLayer1->setShape(textShape);
+  auto strokeStyle1 = SolidColor::Make(Color::White());
+  shapeLayer1->setStrokeStyle(strokeStyle1);
+  shapeLayer1->setLineWidth(5.0f);
+  shapeLayer1->setStrokeAlign(StrokeAlign::Outside);
+  auto fillStyle1 = SolidColor::Make(Color::Blue());
+  shapeLayer1->setFillStyle(fillStyle1);
+  rootLayer->addChild(shapeLayer1);
+
+  auto shapeLayer2 = ShapeLayer::Make();
+  shapeLayer2->setMatrix(Matrix::MakeTrans(0.0f, 150.0f));
+  shapeLayer2->setShape(textShape);
+  auto strokeStyle2 = SolidColor::Make(Color::White());
+  shapeLayer2->setStrokeStyle(strokeStyle2);
+  shapeLayer2->setLineWidth(4.0f);
+  shapeLayer2->setStrokeAlign(StrokeAlign::Center);
+  auto fillStyle2 = SolidColor::Make(Color::Blue());
+  shapeLayer2->setFillStyle(fillStyle2);
+  rootLayer->addChild(shapeLayer2);
+
+  auto shapeLayer3 = ShapeLayer::Make();
+  shapeLayer3->setMatrix(Matrix::MakeTrans(0.0f, 300.0f));
+  shapeLayer3->setShape(textShape);
+  auto strokeStyle3 = SolidColor::Make(Color::White());
+  shapeLayer3->setStrokeStyle(strokeStyle3);
+  shapeLayer3->setLineWidth(2.0f);
+  shapeLayer3->setStrokeAlign(StrokeAlign::Inside);
+  shapeLayer3->setLineDashPattern({4.0f, 4.0f});
+  shapeLayer3->setLineDashPhase(2.0f);
+  auto fillStyle3 = SolidColor::Make(Color::Blue());
+  shapeLayer3->setFillStyle(fillStyle3);
+  rootLayer->addChild(shapeLayer3);
+
+  auto textLayer1 = TextLayer::Make();
+  textLayer1->setMatrix(Matrix::MakeTrans(30.0f, 100.0f));
+  textLayer1->setTextColor(Color::Red());
+  textLayer1->setText("外描边：");
+  textLayer1->setFont(fontText);
+  rootLayer->addChild(textLayer1);
+
+  auto textLayer2 = TextLayer::Make();
+  textLayer2->setMatrix(Matrix::MakeTrans(30.0f, 250.0f));
+  textLayer2->setTextColor(Color::Red());
+  textLayer2->setText("居中描边：");
+  textLayer2->setFont(fontText);
+  rootLayer->addChild(textLayer2);
+
+  auto textLayer3 = TextLayer::Make();
+  textLayer3->setMatrix(Matrix::MakeTrans(30.0f, 400.0f));
+  textLayer3->setTextColor(Color::Red());
+  textLayer3->setText("内描边：");
+  textLayer3->setFont(fontText);
+  rootLayer->addChild(textLayer3);
+
+  // Rect
+  std::vector<GlyphID> glyphIDs2 = {100};
+  std::vector<Point> positions2 = {{0.0f, 0.0f}};
+  auto textBlob2 = TextBlob::MakeFrom(glyphIDs2.data(), positions2.data(), 1, std::make_shared<CustomPathGlyphFace>());
+  auto textShape2 = Shape::MakeFrom(textBlob2);
+
+  auto shapeLayer4 = ShapeLayer::Make();
+  shapeLayer4->setMatrix(Matrix::MakeTrans(150.0f, 550.0f));
+  shapeLayer4->setShape(textShape2);
+  auto strokeStyle4 = SolidColor::Make(Color::White());
+  shapeLayer4->setStrokeStyle(strokeStyle4);
+  shapeLayer4->setLineWidth(10.0f);
+  shapeLayer4->setStrokeAlign(StrokeAlign::Outside);
+  auto fillStyle4 = SolidColor::Make(Color::Blue());
+  shapeLayer4->setFillStyle(fillStyle4);
+  rootLayer->addChild(shapeLayer4);
+
+  auto shapeLayer5 = ShapeLayer::Make();
+  shapeLayer5->setMatrix(Matrix::MakeTrans(350.0f, 550.0f));
+  shapeLayer5->setShape(textShape2);
+  auto strokeStyle5 = SolidColor::Make(Color::White());
+  shapeLayer5->setStrokeStyle(strokeStyle5);
+  shapeLayer5->setLineWidth(10.0f);
+  shapeLayer5->setStrokeAlign(StrokeAlign::Center);
+  auto fillStyle5 = SolidColor::Make(Color::Blue());
+  shapeLayer5->setFillStyle(fillStyle5);
+  rootLayer->addChild(shapeLayer5);
+
+  auto shapeLayer6 = ShapeLayer::Make();
+  shapeLayer6->setMatrix(Matrix::MakeTrans(550.0f, 550.0f));
+  shapeLayer6->setShape(textShape2);
+  auto strokeStyle6 = SolidColor::Make(Color::White());
+  shapeLayer6->setStrokeStyle(strokeStyle6);
+  shapeLayer6->setLineWidth(10.0f);
+  shapeLayer6->setStrokeAlign(StrokeAlign::Inside);
+  auto fillStyle6 = SolidColor::Make(Color::Blue());
+  shapeLayer6->setFillStyle(fillStyle6);
+  rootLayer->addChild(shapeLayer6);
+
+  displayList->root()->addChild(rootLayer);
+  displayList->render(surface.get());
+
+  EXPECT_TRUE(Baseline::Compare(surface, "GlyphFaceTest/MakeTextBlobWithGlyphFace"));
 }
 }  // namespace tgfx
