9b12df621f0fe608930b0630cdc7c6c1c1a7f12c
Hparty <420024556@qq.com>
2025-03-10 22:04:09 +0800

    Fix background changes after layer rasterization without updating related layerStyle. (#496)

--- include/tgfx/layers/Layer.h
+++ include/tgfx/layers/Layer.h
@@ -490,7 +490,7 @@ class Layer {
    * Marks the layer as needing to be redrawn. Unlike invalidateContent(), this method only marks
    * the layer as dirty and does not update the layer content.
    */
-  void invalidate();
+  void invalidateTransform();
 
   /**
    * Marks the layer's content as changed and needing to be redrawn. The updateContent() method will
@@ -531,9 +531,9 @@ class Layer {
 
  private:
   /**
-   * Marks the layer's children as changed and needing to be redrawn.
+   * Marks the layer's descendents as changed and needing to be redrawn.
    */
-  void invalidateChildren();
+  void invalidateDescendents();
 
   void onAttachToRoot(Layer* owner);
 
@@ -581,9 +581,13 @@ class Layer {
 
   bool hasValidMask() const;
 
+  void invalidate();
+
   struct {
-    bool contentDirty : 1;   // need to update content
-    bool childrenDirty : 1;  // need to redraw child layers
+    bool dirtyContent : 1;      // need to update content
+    bool dirtyDescendents : 1;  // need to redraw child layers
+    bool dirtyTransform : 1;    // need to redraw the layer, property such as alpha,
+                                // blendMode, matrix, etc.
     bool visible : 1;
     bool shouldRasterize : 1;
     bool allowsEdgeAntialiasing : 1;
