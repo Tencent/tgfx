1ccae39579d603832cf579e53d4268214188c39c
richardshan0614 <richardshan0614@gmail.com>
2025-06-18 09:43:58 +0800

    The generatePath API in customTypeface shouldn't return false just because the path is empty — for example, space characters are allowed to have empty paths

--- src/core/PathUserTypeface.cpp
+++ src/core/PathUserTypeface.cpp
@@ -60,25 +60,21 @@ class PathUserScalerContext final : public UserScalerContext {
   }
 
   bool generatePath(GlyphID glyphID, bool fauxBold, bool fauxItalic, Path* path) const override {
-    if (path == nullptr) {
-      return false;
-    }
     auto pathProvider = pathTypeFace()->getPathProvider(glyphID);
     if (pathProvider == nullptr) {
+      path->reset();
       return false;
     }
-
     *path = pathProvider->getPath();
-    if (path->isEmpty()) {
-      return false;
-    }
-    auto transform = GetTransform(fauxItalic, textSize);
-    path->transform(transform);
-    if (fauxBold) {
-      auto strokePath = *path;
-      Stroke stroke(textSize * fauxBoldScale);
-      stroke.applyToPath(&strokePath);
-      path->addPath(strokePath, PathOp::Union);
+    if (!path->isEmpty()) {
+      auto transform = GetTransform(fauxItalic, textSize);
+      path->transform(transform);
+      if (fauxBold) {
+        auto strokePath = *path;
+        Stroke stroke(textSize * fauxBoldScale);
+        stroke.applyToPath(&strokePath);
+        path->addPath(strokePath, PathOp::Union);
+      }
     }
     return true;
   }
