785cf9cbc30679d642f9eebedf44b33ba188539c
Dom <dom@idom.me>
2025-06-23 19:35:39 +0800

    Use linear sampling when drawing tiles to the screen in DisplayList.

--- src/layers/DisplayList.cpp
+++ src/layers/DisplayList.cpp
@@ -743,7 +743,7 @@ void DisplayList::drawScreenTasks(std::vector<DrawTask> screenTasks, Surface* su
   if (autoClear) {
     paint.setBlendMode(BlendMode::Src);
   }
-  static SamplingOptions sampling(FilterMode::Nearest, MipmapMode::None);
+  static SamplingOptions sampling(FilterMode::Linear, MipmapMode::None);
   canvas->setMatrix(Matrix::MakeTrans(_contentOffset.x, _contentOffset.y));
   for (auto& task : screenTasks) {
     auto surfaceCache = surfaceCaches[task.sourceIndex()];
