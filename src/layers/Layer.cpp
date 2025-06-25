fd95abad8bcf19b68a948528a5007ec91d262845
Hparty <420024556@qq.com>
2025-06-25 15:02:57 +0800

    bugfix

--- src/layers/Layer.cpp
+++ src/layers/Layer.cpp
@@ -828,9 +828,10 @@ void Layer::drawOffscreen(const DrawArgs& args, Canvas* canvas, float alpha, Ble
     }
     paint.setMaskFilter(maskFilter);
   }
-  auto subBackgroundContext = bitFields.hasBackgroundStyle && args.backgroundContext
-                                  ? args.backgroundContext->createSubContext()
-                                  : nullptr;
+  auto subBackgroundContext =
+      args.backgroundContext && (bitFields.hasBackgroundStyle || hasDescendantBackgroundStyle())
+          ? args.backgroundContext->createSubContext()
+          : nullptr;
   auto offscreenArgs = args;
   offscreenArgs.backgroundContext = subBackgroundContext;
   auto picture = RecordPicture(contentScale,
