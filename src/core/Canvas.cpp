df3c4303758e8f856d7f0699a4a992396bbff5e6
Hparty <420024556@qq.com>
2025-07-09 19:20:32 +0800

    use drawImage instead of drawImageRect in drawLayer

--- src/core/Canvas.cpp
+++ src/core/Canvas.cpp
@@ -588,9 +588,7 @@ void Canvas::drawLayer(std::shared_ptr<Picture> picture, const MCState& state, c
       auto drawState = state;
       drawState.matrix.preTranslate(offset.x + filterOffset.x, offset.y + filterOffset.y);
       auto fillMatrix = Matrix::MakeTrans(-offset.x - filterOffset.x, -offset.y - +filterOffset.y);
-      auto imageRect = Rect::MakeWH(image->width(), image->height());
-      drawContext->drawImageRect(std::move(image), imageRect, imageRect, {}, drawState,
-                                 fill.makeWithMatrix(fillMatrix), SrcRectConstraint::Fast);
+      drawContext->drawImage(std::move(image), {}, drawState, fill.makeWithMatrix(fillMatrix));
       return;
     }
   } else if (picture->drawCount == 1 && fill.maskFilter == nullptr) {
