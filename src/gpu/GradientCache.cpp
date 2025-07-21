88982f0242a1b5e3c0f7c7cf0791e96d64ad3d23
thunderllei <thunderllei@tencent.com>
2025-07-21 20:18:29 +0800

    fix code

--- src/gpu/GradientCache.cpp
+++ src/gpu/GradientCache.cpp
@@ -43,8 +43,53 @@ void GradientCache::add(const BytesKey& bytesKey, std::shared_ptr<Texture> textu
     textures.erase(key);
   }
 }
-extern std::shared_ptr<ImageBuffer> CreateGradient(const Color* colors, const float* positions,
-                                                   int count, int resolution);
+
+std::shared_ptr<ImageBuffer> CreateGradient(const Color* colors, const float* positions, int count,
+                                            int resolution) {
+  Bitmap bitmap(resolution, 1, false, false);
+  Pixmap pixmap(bitmap);
+  if (pixmap.isEmpty()) {
+    return nullptr;
+  }
+  pixmap.clear();
+  auto pixels = reinterpret_cast<uint8_t*>(pixmap.writablePixels());
+  int prevIndex = 0;
+  for (int i = 1; i < count; ++i) {
+    int nextIndex =
+        std::min(static_cast<int>(positions[i] * static_cast<float>(resolution)), resolution - 1);
+
+    if (nextIndex > prevIndex) {
+      auto r0 = colors[i - 1].red;
+      auto g0 = colors[i - 1].green;
+      auto b0 = colors[i - 1].blue;
+      auto a0 = colors[i - 1].alpha;
+      auto r1 = colors[i].red;
+      auto g1 = colors[i].green;
+      auto b1 = colors[i].blue;
+      auto a1 = colors[i].alpha;
+
+      auto step = 1.0f / static_cast<float>(nextIndex - prevIndex);
+      auto deltaR = (r1 - r0) * step;
+      auto deltaG = (g1 - g0) * step;
+      auto deltaB = (b1 - b0) * step;
+      auto deltaA = (a1 - a0) * step;
+
+      for (int curIndex = prevIndex; curIndex <= nextIndex; ++curIndex) {
+        pixels[curIndex * 4] = static_cast<uint8_t>(r0 * 255.0f);
+        pixels[curIndex * 4 + 1] = static_cast<uint8_t>(g0 * 255.0f);
+        pixels[curIndex * 4 + 2] = static_cast<uint8_t>(b0 * 255.0f);
+        pixels[curIndex * 4 + 3] = static_cast<uint8_t>(a0 * 255.0f);
+        r0 += deltaR;
+        g0 += deltaG;
+        b0 += deltaB;
+        a0 += deltaA;
+      }
+    }
+    prevIndex = nextIndex;
+  }
+  return bitmap.makeBuffer();
+}
+
 std::shared_ptr<Texture> GradientCache::getGradient(Context* context, const Color* colors,
                                                     const float* positions, int count) {
   BytesKey bytesKey = {};
