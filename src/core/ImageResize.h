356fcd7b9224d8d8a11cff58c0776f6490cc1391
thunderllei <thunderllei@tencent.com>
2025-07-29 20:03:46 +0800

    add boxFilterDownSampling function

--- src/core/ImageResize.h
+++ src/core/ImageResize.h
@@ -20,55 +20,9 @@
 
 namespace tgfx {
 
-// PixelLayout specifies:
-//  number of channels
-//  order of channels
-//  whether color is premultiplied by alpha
-enum class PixelLayout {
-  CHANNEL_1 = 1,
-  CHANNEL_2 = 2,
-  // 3-chan, with order specified (for channel flipping)
-  RGB = 3,
-  BGR = 0,
-  CHANNEL_4 = 5,
+enum class PixelLayout { CHANNEL_1 = 1, RGB = 3, RGBA = 4 };
 
-  // alpha formats, where alpha is NOT premultiplied into color channels
-  RGBA = 4,
-  BGRA = 6,
-  ARGB = 7,
-  ABGR = 8,
-  RA = 9,
-  AR = 10,
-
-  // alpha formats, where alpha is premultiplied into color channels
-  RGBA_PM = 11,
-  BGRA_PM = 12,
-  ARGB_PM = 13,
-  ABGR_PM = 14,
-  RA_PM = 15,
-  AR_PM = 16,
-
-  // alpha formats, where NO alpha weighting is applied at all! these are just synonyms for the _PM
-  // flags (which also do no alpha weighting). These names just make it more clear for some folks.
-  RGBA_NO_AW = 11,
-  BGRA_NO_AW = 12,
-  ARGB_NO_AW = 13,
-  ABGR_NO_AW = 14,
-  RA_NO_AW = 15,
-  AR_NO_AW = 16
-};
-
-enum class DataType {
-  UINT8 = 0,
-  UINT8_SRGB = 1,
-  // alpha channel, when present, should also be SRGB (this is very unusual)
-  UINT8_SRGB_ALPHA = 2,
-  UINT16 = 3,
-  FLOAT = 4,
-  HALF_FLOAT = 5
-};
-
-bool ImageResize(const void* inputPixels, int inputW, int inputH, int inputStrideInBytes,
-                 void* outputPixels, int outputW, int outputH, int outputStrideInBytes,
-                 PixelLayout pixelLayout, DataType dataType);
+void boxFilterDownSampling(const void* inputPixels, int inputW, int inputH, int inputStrideInBytes,
+                           void* outputPixels, int outputW, int outputH, int outputStrideInBytes,
+                           PixelLayout pixelLayout);
 }  // namespace tgfx
