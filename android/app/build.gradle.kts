4a0376fe6ce8cf4693d402b855346f9c2d34ab8b
YGauroa <neilgyang@tencent.com>
2025-07-30 10:41:03 +0800

    xx

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 
             val cmakeArgs: String? = project.findProperty("cmakeArgs") as String?
             if (!cmakeArgs.isNullOrEmpty()) {
-                arguments.addAll(cmakeArgs.split(" "))
+                externalNativeBuild.cmake.arguments.addAll(cmakeArgs.split(" "))
             }
         }
     }
