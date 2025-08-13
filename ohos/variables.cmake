61f8ed8860a54af1a8da73af5f1eba1debefb3f8
codywwang <codywwang@tencent.com>
2025-08-13 11:20:06 +0800

    Add openHarmony workflow

---
+++ ohos/variables.cmake
@@ -0,0 +1,2 @@
+set(PIXELMAP_LIB "/path/to/libpixelmap_ndk.z.so" CACHE FILEPATH "Path to PixelMap Library")
+set(IMAGE_SOURCE_LIB "/path/to/libimage_source_ndk.z.so" CACHE FILEPATH "Path to Image Source Library")
