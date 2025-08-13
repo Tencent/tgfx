a0b0e8e8be4422660358c42ea7f8e7d65f4d25d9
junhaoduan <junhaoduan@tencent.com>
2025-08-13 14:20:33 +0800

    DrawerTest apdat

--- test/src/DrawersTest.cpp
+++ test/src/DrawersTest.cpp
@@ -24,6 +24,7 @@ namespace tgfx {
 TGFX_TEST(DrawersTest, Compare) {
   drawers::AppHost appHost(720, 720, 2.0f);
   appHost.addImage("bridge", MakeImage("resources/assets/bridge.jpg"));
+  appHost.addImage("TGFX", MakeImage("resources/assets/tgfx.png"));
   appHost.addTypeface("default", MakeTypeface("resources/font/NotoSansSC-Regular.otf"));
   appHost.addTypeface("emoji", MakeTypeface("resources/font/NotoColorEmoji.ttf"));
 
@@ -37,13 +38,22 @@ TGFX_TEST(DrawersTest, Compare) {
     auto drawer = drawers::Drawer::GetByName(name);
     ASSERT_TRUE(drawer != nullptr);
     canvas->clear();
-    drawer->draw(canvas, &appHost);
+    drawer->build(&appHost);
+    drawer->displayList.render(canvas->getSurface(), false);
     auto key = "DrawersTest/" + name;
     auto result = Baseline::Compare(surface, key);
     if (!result) {
       ADD_FAILURE();
       LOGI("Baseline::Compare failed for %s", key.c_str());
     }
+  }
+  canvas->clear();
+  drawers::Drawer::DrawBackground(canvas, &appHost);
+  std::string key = "DrawersTest/GridBackGround";
+  auto result = Baseline::Compare(surface, key);
+  if (!result) {
+    ADD_FAILURE();
+    LOGI("Baseline::Compare failed for %s", key.c_str());
   }
 }
 }  // namespace tgfx
