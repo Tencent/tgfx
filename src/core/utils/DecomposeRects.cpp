1c51ab24d8f3d746f7ade997c9d62edc862e4636
thunderllei <thunderllei@tencent.com>
2025-11-18 22:12:02 +0800

    fix error.

--- src/core/utils/DecomposeRects.cpp
+++ src/core/utils/DecomposeRects.cpp
@@ -35,7 +35,6 @@ static void DecomposeRect(Rect* rectA, Rect* rectB) {
     rects[0].top = rectB->top;
     rects[0].bottom = rectA->top;
     rects[0].left = rectB->left;
-
     rects[0].right = rectB->right;
   }
   if (rectA->bottom < rectB->bottom) {
