d231f69253af3bd07600527f36be93174297b476
Dom Chen <dom@idom.me>
2025-11-21 14:30:01 +0800

    Add a Recording class for deferred submission and update flush() and submit() methods of Context accordingly. (#1056)

--- src/gpu/DrawingManager.cpp
+++ src/gpu/DrawingManager.cpp
@@ -19,7 +19,6 @@
 #include "DrawingManager.h"
 #include "ProxyProvider.h"
 #include "core/AtlasManager.h"
-#include "core/utils/HardwareBufferUtil.h"
 #include "gpu/proxies/RenderTargetProxy.h"
 #include "gpu/proxies/TextureProxy.h"
 #include "gpu/tasks/GenerateMipmapsTask.h"
@@ -27,11 +26,22 @@
 #include "gpu/tasks/RuntimeDrawTask.h"
 #include "inspect/InspectorMark.h"
 #include "tasks/TransferPixelsTask.h"
-#include "tgfx/gpu/GPU.h"
 
 namespace tgfx {
-DrawingManager::DrawingManager(Context* context)
-    : context(context), drawingAllocator(context->drawingAllocator()) {
+DrawingManager::DrawingManager(Context* context) : context(context) {
+}
+
+DrawingBuffer* DrawingManager::createDrawingBuffer() {
+  for (auto it = bufferPool.begin(); it != bufferPool.end(); ++it) {
+    if (it->use_count() == 1) {
+      currentBuffer = *it;
+      bufferPool.erase(it);
+      currentBuffer->reset();
+      return currentBuffer.get();
+    }
+  }
+  currentBuffer = std::make_shared<DrawingBuffer>(context);
+  return currentBuffer.get();
 }
 
 bool DrawingManager::fillRTWithFP(std::shared_ptr<RenderTargetProxy> renderTarget,
@@ -39,16 +49,18 @@ bool DrawingManager::fillRTWithFP(std::shared_ptr<RenderTargetProxy> renderTarge
   if (renderTarget == nullptr || processor == nullptr) {
     return false;
   }
+  auto drawingBuffer = getDrawingBuffer();
+  auto allocator = &drawingBuffer->drawingAllocator;
   auto bounds = Rect::MakeWH(renderTarget->width(), renderTarget->height());
-  auto provider = RectsVertexProvider::MakeFrom(drawingAllocator, bounds, AAType::None);
+  auto provider = RectsVertexProvider::MakeFrom(allocator, bounds, AAType::None);
   auto drawOp = RectDrawOp::Make(renderTarget->getContext(), std::move(provider), renderFlags);
   drawOp->addColorFP(std::move(processor));
   drawOp->setBlendMode(BlendMode::Src);
-  auto drawOps = drawingAllocator->makeArray<DrawOp>(&drawOp, 1);
+  auto drawOps = allocator->makeArray<DrawOp>(&drawOp, 1);
   auto textureProxy = renderTarget->asTextureProxy();
-  auto task = drawingAllocator->make<OpsRenderTask>(drawingAllocator, std::move(renderTarget),
-                                                    std::move(drawOps), std::nullopt);
-  renderTasks.emplace_back(std::move(task));
+  auto task = allocator->make<OpsRenderTask>(allocator, std::move(renderTarget), std::move(drawOps),
+                                             std::nullopt);
+  drawingBuffer->renderTasks.emplace_back(std::move(task));
   addGenerateMipmapsTask(std::move(textureProxy));
   return true;
 }
@@ -69,10 +81,12 @@ void DrawingManager::addOpsRenderTask(std::shared_ptr<RenderTargetProxy> renderT
   if (renderTarget == nullptr || (drawOps.empty() && !clearColor.has_value())) {
     return;
   }
+  auto drawingBuffer = getDrawingBuffer();
+  auto allocator = &drawingBuffer->drawingAllocator;
   auto textureProxy = renderTarget->asTextureProxy();
-  auto task = drawingAllocator->make<OpsRenderTask>(drawingAllocator, std::move(renderTarget),
-                                                    std::move(drawOps), clearColor);
-  renderTasks.emplace_back(std::move(task));
+  auto task = allocator->make<OpsRenderTask>(allocator, std::move(renderTarget), std::move(drawOps),
+                                             clearColor);
+  drawingBuffer->renderTasks.emplace_back(std::move(task));
   addGenerateMipmapsTask(std::move(textureProxy));
 }
 
@@ -83,10 +97,12 @@ void DrawingManager::addRuntimeDrawTask(std::shared_ptr<RenderTargetProxy> rende
   if (renderTarget == nullptr || inputs.empty() || effect == nullptr) {
     return;
   }
+  auto drawingBuffer = getDrawingBuffer();
+  auto allocator = &drawingBuffer->drawingAllocator;
   auto textureProxy = renderTarget->asTextureProxy();
-  auto task = drawingAllocator->make<RuntimeDrawTask>(drawingAllocator, std::move(renderTarget),
-                                                      std::move(inputs), std::move(effect), offset);
-  renderTasks.emplace_back(std::move(task));
+  auto task = allocator->make<RuntimeDrawTask>(allocator, std::move(renderTarget),
+                                               std::move(inputs), std::move(effect), offset);
+  drawingBuffer->renderTasks.emplace_back(std::move(task));
   addGenerateMipmapsTask(std::move(textureProxy));
 }
 
@@ -94,9 +110,10 @@ void DrawingManager::addGenerateMipmapsTask(std::shared_ptr<TextureProxy> textur
   if (textureProxy == nullptr || !textureProxy->hasMipmaps()) {
     return;
   }
-  auto task =
-      drawingAllocator->make<GenerateMipmapsTask>(drawingAllocator, std::move(textureProxy));
-  renderTasks.emplace_back(std::move(task));
+  auto drawingBuffer = getDrawingBuffer();
+  auto allocator = &drawingBuffer->drawingAllocator;
+  auto task = allocator->make<GenerateMipmapsTask>(allocator, std::move(textureProxy));
+  drawingBuffer->renderTasks.emplace_back(std::move(task));
 }
 
 void DrawingManager::addRenderTargetCopyTask(std::shared_ptr<RenderTargetProxy> source,
@@ -105,9 +122,11 @@ void DrawingManager::addRenderTargetCopyTask(std::shared_ptr<RenderTargetProxy> 
   if (source == nullptr || dest == nullptr) {
     return;
   }
-  auto task = drawingAllocator->make<RenderTargetCopyTask>(drawingAllocator, std::move(source),
-                                                           std::move(dest), srcX, srcY);
-  renderTasks.emplace_back(std::move(task));
+  auto drawingBuffer = getDrawingBuffer();
+  auto allocator = &drawingBuffer->drawingAllocator;
+  auto task = allocator->make<RenderTargetCopyTask>(allocator, std::move(source), std::move(dest),
+                                                    srcX, srcY);
+  drawingBuffer->renderTasks.emplace_back(std::move(task));
 }
 
 void DrawingManager::addTransferPixelsTask(std::shared_ptr<RenderTargetProxy> source,
@@ -116,16 +135,19 @@ void DrawingManager::addTransferPixelsTask(std::shared_ptr<RenderTargetProxy> so
   if (source == nullptr || dest == nullptr || srcRect.isEmpty()) {
     return;
   }
-  auto task = drawingAllocator->make<TransferPixelsTask>(drawingAllocator, std::move(source),
-                                                         srcRect, std::move(dest));
-  renderTasks.emplace_back(std::move(task));
+  auto drawingBuffer = getDrawingBuffer();
+  auto allocator = &drawingBuffer->drawingAllocator;
+  auto task =
+      allocator->make<TransferPixelsTask>(allocator, std::move(source), srcRect, std::move(dest));
+  drawingBuffer->renderTasks.emplace_back(std::move(task));
 }
 
 void DrawingManager::addResourceTask(PlacementPtr<ResourceTask> resourceTask) {
   if (resourceTask == nullptr) {
     return;
   }
-  resourceTasks.emplace_back(std::move(resourceTask));
+  auto drawingBuffer = getDrawingBuffer();
+  drawingBuffer->resourceTasks.emplace_back(std::move(resourceTask));
 }
 
 void DrawingManager::addAtlasCellTask(std::shared_ptr<TextureProxy> textureProxy,
@@ -133,55 +155,43 @@ void DrawingManager::addAtlasCellTask(std::shared_ptr<TextureProxy> textureProxy
   if (textureProxy == nullptr || codec == nullptr) {
     return;
   }
+  auto drawingBuffer = getDrawingBuffer();
+  auto allocator = &drawingBuffer->drawingAllocator;
   AtlasUploadTask* atlasUploadTask = nullptr;
-  auto result = atlasTasks.find(textureProxy.get());
-  if (result != atlasTasks.end()) {
-    atlasUploadTask = result->second.get();
+  auto taskKey = textureProxy.get();
+  auto result = atlasTaskMap.find(taskKey);
+  if (result != atlasTaskMap.end()) {
+    atlasUploadTask = result->second;
   } else {
-    auto atlasTask = drawingAllocator->make<AtlasUploadTask>(textureProxy);
+    auto atlasTask = allocator->make<AtlasUploadTask>(std::move(textureProxy));
     atlasUploadTask = atlasTask.get();
-    atlasTasks[textureProxy.get()] = std::move(atlasTask);
+    drawingBuffer->atlasTasks.emplace_back(std::move(atlasTask));
+    atlasTaskMap[taskKey] = atlasUploadTask;
   }
-  atlasUploadTask->addCell(drawingAllocator, std::move(codec), atlasOffset);
+  atlasUploadTask->addCell(allocator, std::move(codec), atlasOffset);
 }
 
-std::shared_ptr<CommandBuffer> DrawingManager::flush() {
-  TASK_MARK(tgfx::inspect::OpTaskType::Flush);
+std::shared_ptr<DrawingBuffer> DrawingManager::flush() {
+  if (currentBuffer == nullptr) {
+    return nullptr;
+  }
   while (!compositors.empty()) {
     auto compositor = compositors.back();
     // The makeClosed() method may add more compositors to the list.
     compositor->makeClosed();
   }
-  auto proxyProvider = context->proxyProvider();
   // Flush the shared vertex buffer before executing the tasks. It may generate new resource tasks.
-  proxyProvider->flushSharedVertexBuffer();
+  context->proxyProvider()->flushSharedVertexBuffer();
+  atlasTaskMap.clear();
 
-  if (resourceTasks.empty() && renderTasks.empty() && atlasTasks.empty()) {
-    proxyProvider->clearSharedVertexBuffer();
+  if (currentBuffer->empty()) {
+    currentBuffer->reset();
     return nullptr;
   }
-  {
-    TASK_MARK(tgfx::inspect::OpTaskType::ResourceTask);
-    for (auto& task : resourceTasks) {
-      task->execute(context);
-      task = nullptr;
-    }
-  }
-  resourceTasks.clear();
-  for (auto& task : atlasTasks) {
-    task.second->upload(context);
-  }
-  atlasTasks.clear();
-  proxyProvider->clearSharedVertexBuffer();
-  auto commandEncoder = context->gpu()->createCommandEncoder();
-  {
-    TASK_MARK(tgfx::inspect::OpTaskType::RenderTask);
-    for (auto& task : renderTasks) {
-      task->execute(commandEncoder.get());
-      task = nullptr;
-    }
-  }
-  renderTasks.clear();
-  return commandEncoder->finish();
+
+  auto drawingBuffer = currentBuffer;
+  bufferPool.push_back(currentBuffer);
+  currentBuffer = nullptr;
+  return drawingBuffer;
 }
 }  // namespace tgfx
