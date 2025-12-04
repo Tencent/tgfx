43f91b9d5a14d7723764e4d911bc3f69ff14e0fe
Dom <dom@idom.me>
2025-12-04 13:08:55 +0800

    Remove TextBlob::getPath() and improve Shape::MakeFrom(TextBlob) to merge all outline glyphs.

--- src/core/TextBlob.cpp
+++ src/core/TextBlob.cpp
@@ -137,13 +137,4 @@ Rect TextBlob::getTightBounds(const Matrix* matrix) const {
   }
   return bounds;
 }
-
-bool TextBlob::getPath(Path* path, const Matrix* matrix) const {
-  if (glyphRunLists.size() != 1) {
-    // If there are multiple glyph run lists, meaning the text blob has multiple font types,
-    // only one font type can be used to create a path.
-    return false;
-  }
-  return glyphRunLists.front()->getPath(path, matrix);
-}
 }  // namespace tgfx
