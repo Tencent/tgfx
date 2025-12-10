3b9c2c789b1c13f4ea53e7ae4836c8e7928a06f7
leiyue123 <48425418+leiyue123@users.noreply.github.com>
2025-12-10 19:26:14 +0800

    Add assignColorSpace field to PDFMetadata for PDF color profile embedding. (#1096)

--- src/pdf/PDFDocumentImpl.cpp
+++ src/pdf/PDFDocumentImpl.cpp
@@ -344,7 +344,7 @@ Canvas* PDFDocumentImpl::onBeginPage(float width, float height) {
     // if this is the first page if the document.
     SerializeHeader(&offsetMap, _stream);
     infoDictionary = this->emit(*PDFMetadataUtils::MakeDocumentInformationDict(_metadata));
-    if (_metadata.colorSpace != nullptr) {
+    if (_metadata.targetColorSpace != nullptr || _metadata.assignColorSpace != nullptr) {
       _colorSpaceRef = emitColorSpace();
     }
     if (_metadata.PDFA) {
@@ -499,11 +499,13 @@ void PDFDocumentImpl::endObject() {
 }
 
 PDFIndirectReference PDFDocumentImpl::emitColorSpace() {
-  DEBUG_ASSERT(_metadata.colorSpace != nullptr);
+  DEBUG_ASSERT(_metadata.targetColorSpace != nullptr || _metadata.assignColorSpace != nullptr);
+  auto writeColorSpace =
+      _metadata.assignColorSpace ? _metadata.assignColorSpace : _metadata.targetColorSpace;
   auto colorSpaceDictionary = PDFDictionary::Make();
   colorSpaceDictionary->insertInt("N", 3);
   colorSpaceDictionary->insertName("Alternate", "DeviceRGB");
-  auto iccProfile = _metadata.colorSpace->toICCProfile();
+  auto iccProfile = writeColorSpace->toICCProfile();
   auto stream = Stream::MakeFromData(iccProfile);
   auto ref = PDFStreamOut(std::move(colorSpaceDictionary), std::move(stream), this);
   PDFArray array{};
