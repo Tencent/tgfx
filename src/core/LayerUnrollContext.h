49aeb5678b05e3493e8cf6c297528664ef42e915
Dom <dom@idom.me>
2025-12-11 14:40:04 +0800

    Refactor layer unroll logic into a separate LayerUnrollContext class.

---
+++ src/core/LayerUnrollContext.h
@@ -0,0 +1,74 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Tencent is pleased to support the open source community by making tgfx available.
+//
+//  Copyright (C) 2025 Tencent. All rights reserved.
+//
+//  Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+//  in compliance with the License. You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/BSD-3-Clause
+//
+//  unless required by applicable law or agreed to in writing, software distributed under the
+//  license is distributed on an "as is" basis, without warranties or conditions of any kind,
+//  either express or implied. see the license for the specific language governing permissions
+//  and limitations under the license.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "core/DrawContext.h"
+
+namespace tgfx {
+/**
+ * LayerUnrollContext is a DrawContext proxy that intercepts draw calls and merges layer brush
+ * properties (alpha, blendMode, colorFilter) into each draw command's brush. This allows a layer
+ * containing a single draw command to be "unrolled" and drawn directly without creating an
+ * offscreen buffer, improving performance by avoiding unnecessary layer allocations.
+ */
+class LayerUnrollContext : public DrawContext {
+ public:
+  LayerUnrollContext(DrawContext* drawContext, Brush layerBrush);
+
+  bool hasUnrolled() const {
+    return unrolled;
+  }
+
+  void drawFill(const Brush& brush) override;
+
+  void drawRect(const Rect& rect, const MCState& state, const Brush& brush,
+                const Stroke* stroke) override;
+
+  void drawRRect(const RRect& rRect, const MCState& state, const Brush& brush,
+                 const Stroke* stroke) override;
+
+  void drawPath(const Path& path, const MCState& state, const Brush& brush) override;
+
+  void drawShape(std::shared_ptr<Shape> shape, const MCState& state, const Brush& brush,
+                 const Stroke* stroke) override;
+
+  void drawImage(std::shared_ptr<Image> image, const SamplingOptions& sampling,
+                 const MCState& state, const Brush& brush) override;
+
+  void drawImageRect(std::shared_ptr<Image> image, const Rect& srcRect, const Rect& dstRect,
+                     const SamplingOptions& sampling, const MCState& state, const Brush& brush,
+                     SrcRectConstraint constraint) override;
+
+  void drawGlyphRunList(std::shared_ptr<GlyphRunList> glyphRunList, const MCState& state,
+                        const Brush& brush, const Stroke* stroke) override;
+
+  void drawPicture(std::shared_ptr<Picture> picture, const MCState& state) override;
+
+  void drawLayer(std::shared_ptr<Picture> picture, std::shared_ptr<ImageFilter> filter,
+                 const MCState& state, const Brush& brush) override;
+
+ protected:
+  Brush mergeBrush(const Brush& brush) const;
+
+ private:
+  DrawContext* drawContext = nullptr;
+  Brush layerBrush = {};
+  bool unrolled = false;
+};
+}  // namespace tgfx
