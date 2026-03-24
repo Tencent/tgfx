0ae5f941ae77406b0c2c5b798a6c84608e6696b4
StarryThrone <richardjiechen@gmail.com>
2026-03-24 19:55:12 +0800

    Fix copyright year in SmallVector.h from 2023 to 2026.

--- src/core/SmallVector.h
+++ src/core/SmallVector.h
@@ -2,7 +2,7 @@
 //
 //  Tencent is pleased to support the open source community by making tgfx available.
 //
-//  Copyright (C) 2023 Tencent. All rights reserved.
+//  Copyright (C) 2026 Tencent. All rights reserved.
 //
 //  Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
 //  in compliance with the License. You may obtain a copy of the License at
