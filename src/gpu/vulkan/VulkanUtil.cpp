052bd4548f799b6babdeb277aeaabd4735b15b86
edwardxfshen <xianfeishen@qq.com>
2026-04-29 11:04:36 +0800

    Complete Phase 2 and Phase 3A of Vulkan backend implementation.

---
+++ src/gpu/vulkan/VulkanUtil.cpp
@@ -0,0 +1,142 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Tencent is pleased to support the open source community by making tgfx available.
+//
+//  Copyright (C) 2026 Tencent. All rights reserved.
+//
+//  Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+//  in compliance with the License. You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/BSD-3-Clause
+//
+//  unless required by applicable law or agreed to in writing, software distributed under the
+//  license is distributed on an "as is" basis, without warranties or conditions of any kind,
+//  either express or implied. see the license for the specific language governing permissions
+//  and limitations under the license.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "VulkanUtil.h"
+#include "core/utils/Log.h"
+
+namespace tgfx {
+
+const char* VkResultToString(VkResult result) {
+  switch (result) {
+    case VK_SUCCESS:
+      return "VK_SUCCESS";
+    case VK_NOT_READY:
+      return "VK_NOT_READY";
+    case VK_TIMEOUT:
+      return "VK_TIMEOUT";
+    case VK_EVENT_SET:
+      return "VK_EVENT_SET";
+    case VK_EVENT_RESET:
+      return "VK_EVENT_RESET";
+    case VK_INCOMPLETE:
+      return "VK_INCOMPLETE";
+    case VK_ERROR_OUT_OF_HOST_MEMORY:
+      return "VK_ERROR_OUT_OF_HOST_MEMORY";
+    case VK_ERROR_OUT_OF_DEVICE_MEMORY:
+      return "VK_ERROR_OUT_OF_DEVICE_MEMORY";
+    case VK_ERROR_INITIALIZATION_FAILED:
+      return "VK_ERROR_INITIALIZATION_FAILED";
+    case VK_ERROR_DEVICE_LOST:
+      return "VK_ERROR_DEVICE_LOST";
+    case VK_ERROR_MEMORY_MAP_FAILED:
+      return "VK_ERROR_MEMORY_MAP_FAILED";
+    case VK_ERROR_LAYER_NOT_PRESENT:
+      return "VK_ERROR_LAYER_NOT_PRESENT";
+    case VK_ERROR_EXTENSION_NOT_PRESENT:
+      return "VK_ERROR_EXTENSION_NOT_PRESENT";
+    case VK_ERROR_FEATURE_NOT_PRESENT:
+      return "VK_ERROR_FEATURE_NOT_PRESENT";
+    case VK_ERROR_INCOMPATIBLE_DRIVER:
+      return "VK_ERROR_INCOMPATIBLE_DRIVER";
+    case VK_ERROR_TOO_MANY_OBJECTS:
+      return "VK_ERROR_TOO_MANY_OBJECTS";
+    case VK_ERROR_FORMAT_NOT_SUPPORTED:
+      return "VK_ERROR_FORMAT_NOT_SUPPORTED";
+    case VK_ERROR_FRAGMENTED_POOL:
+      return "VK_ERROR_FRAGMENTED_POOL";
+    case VK_ERROR_SURFACE_LOST_KHR:
+      return "VK_ERROR_SURFACE_LOST_KHR";
+    case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:
+      return "VK_ERROR_NATIVE_WINDOW_IN_USE_KHR";
+    case VK_SUBOPTIMAL_KHR:
+      return "VK_SUBOPTIMAL_KHR";
+    case VK_ERROR_OUT_OF_DATE_KHR:
+      return "VK_ERROR_OUT_OF_DATE_KHR";
+    default:
+      return "VK_UNKNOWN_ERROR";
+  }
+}
+
+VkFormat PixelFormatToVkFormat(PixelFormat format) {
+  switch (format) {
+    case PixelFormat::ALPHA_8:
+      return VK_FORMAT_R8_UNORM;
+    case PixelFormat::GRAY_8:
+      return VK_FORMAT_R8_UNORM;
+    case PixelFormat::RG_88:
+      return VK_FORMAT_R8G8_UNORM;
+    case PixelFormat::RGBA_8888:
+      return VK_FORMAT_R8G8B8A8_UNORM;
+    case PixelFormat::BGRA_8888:
+      return VK_FORMAT_B8G8R8A8_UNORM;
+    case PixelFormat::DEPTH24_STENCIL8:
+      return VK_FORMAT_D24_UNORM_S8_UINT;
+    default:
+      return VK_FORMAT_UNDEFINED;
+  }
+}
+
+PixelFormat VkFormatToPixelFormat(VkFormat format) {
+  switch (format) {
+    case VK_FORMAT_R8_UNORM:
+      return PixelFormat::ALPHA_8;
+    case VK_FORMAT_R8G8_UNORM:
+      return PixelFormat::RG_88;
+    case VK_FORMAT_R8G8B8A8_UNORM:
+    case VK_FORMAT_R8G8B8A8_SRGB:
+      return PixelFormat::RGBA_8888;
+    case VK_FORMAT_B8G8R8A8_UNORM:
+    case VK_FORMAT_B8G8R8A8_SRGB:
+      return PixelFormat::BGRA_8888;
+    case VK_FORMAT_D24_UNORM_S8_UINT:
+    case VK_FORMAT_D32_SFLOAT_S8_UINT:
+      return PixelFormat::DEPTH24_STENCIL8;
+    default:
+      return PixelFormat::Unknown;
+  }
+}
+
+bool VkFormatIsDepthStencil(VkFormat format) {
+  return format == VK_FORMAT_D24_UNORM_S8_UINT || format == VK_FORMAT_D32_SFLOAT_S8_UINT;
+}
+
+bool VkFormatIsStencil(VkFormat format) {
+  return format == VK_FORMAT_D24_UNORM_S8_UINT || format == VK_FORMAT_D32_SFLOAT_S8_UINT ||
+         format == VK_FORMAT_S8_UINT;
+}
+
+int VkFormatBytesPerPixel(VkFormat format) {
+  switch (format) {
+    case VK_FORMAT_R8_UNORM:
+      return 1;
+    case VK_FORMAT_R8G8_UNORM:
+      return 2;
+    case VK_FORMAT_R8G8B8A8_UNORM:
+    case VK_FORMAT_R8G8B8A8_SRGB:
+    case VK_FORMAT_B8G8R8A8_UNORM:
+    case VK_FORMAT_B8G8R8A8_SRGB:
+    case VK_FORMAT_D24_UNORM_S8_UINT:
+      return 4;
+    case VK_FORMAT_D32_SFLOAT_S8_UINT:
+      return 5;
+    default:
+      return 0;
+  }
+}
+
+}  // namespace tgfx
