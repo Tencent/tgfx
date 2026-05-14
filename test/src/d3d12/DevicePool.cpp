c6527ae6a44e84160559a4171c6f6a5d03198261
thunderllei <15002975366@163.com>
2026-05-14 16:38:58 +0800

    Add D3D12 test DevicePool to wire up test infrastructure with D3D12 backend.

---
+++ test/src/d3d12/DevicePool.cpp
@@ -0,0 +1,31 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Tencent is pleased to support the open source community by making tgfx available.
+//
+//  Copyright (C) 2026 Tencent. All rights reserved.
+//
+//  Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+//  in compliance with the License. You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/BSD-3-Clause
+//
+//  unless required by applicable law or agreed to in writing, software distributed under the
+//  license is distributed on an "as is" basis, without warranties or conditions of any kind,
+//  either express or implied. see the license for the specific language governing permissions
+//  and limitations under the license.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "tgfx/gpu/d3d12/D3D12Device.h"
+#include "utils/DevicePool.h"
+
+namespace tgfx {
+thread_local std::shared_ptr<Device> cachedDevice = nullptr;
+
+std::shared_ptr<Device> DevicePool::Make() {
+  if (cachedDevice == nullptr) {
+    cachedDevice = D3D12Device::Make();
+  }
+  return cachedDevice;
+}
+}  // namespace tgfx
