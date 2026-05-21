40e64dcc90c91ff5253c0d970c06a9e9bcdf61d8
edwardxfshen <xianfeishen@qq.com>
2026-05-21 19:36:59 +0800

    Add npm global prefix to PATH in update_baseline.bat to fix depsync not found on CI.

--- update_baseline.bat
+++ update_baseline.bat
@@ -56,6 +56,7 @@ git stash push --include-untracked --quiet
 git switch main --quiet
 
 :: Install dependencies
+for /f "delims=" %%i in ('npm prefix -g') do set "PATH=%%i;!PATH!"
 call npm install depsync -g >nul 2>&1
 call depsync
 
