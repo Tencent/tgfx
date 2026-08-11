54c474aa3d6b3fb1923e9a3645ba368e88380908
beiaoHuang <beiaoHuang@tencent.com>
2026-08-11 15:54:39 +0800

    Create mipmaps only for minified affine 3D leaves.

--- src/layers/compositing3d/Render3DContext.h
+++ src/layers/compositing3d/Render3DContext.h
@@ -64,6 +64,7 @@ class Render3DContext : public Layer3DContext {
     Matrix density = Matrix::I();
     int rasterWidth = 0;
     int rasterHeight = 0;
+    bool needsMipmaps = true;
   };
 
   void emitNode(Layer* layer, const Rect& localBounds, const Matrix3D& transform, float alpha,
