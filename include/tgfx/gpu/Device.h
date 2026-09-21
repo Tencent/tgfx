622eb3ae52d1775ce20b264c0d8f39f0c6bb6f69
thunderllei <thunderllei@tencent.com>
2026-09-21 11:05:03 +0800

    Make Device GetAllNative public for the EAGL background flush entry.

--- include/tgfx/gpu/Device.h
+++ include/tgfx/gpu/Device.h
@@ -57,6 +57,12 @@ class Device {
    */
   void unlock();
 
+  /**
+   * Returns a snapshot of all live registered devices. Devices are registered via
+   * RegisterNative() (see the protected section) as their backends create them.
+   */
+  static std::vector<std::shared_ptr<Device>> GetAllNative();
+
  protected:
   std::mutex locker = {};
   GPU* _gpu = nullptr;
@@ -87,11 +93,6 @@ class Device {
    * none.
    */
   static std::shared_ptr<Device> FindNative(const DeviceKey& key);
-
-  /**
-   * Returns a snapshot of all live registered devices.
-   */
-  static std::vector<std::shared_ptr<Device>> GetAllNative();
 
  private:
   uint32_t _uniqueID = 0;
